398cfd24cf459c4cb4b6aaee0c869a14a12fd1c0
Person317 <gemiyah317@gmail.com>
2024-12-02 12:21:59 -0500

    Importing data

---
+++ Data.Sci.py
@@ -0,0 +1,16 @@
+
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("prasertk/netflix-subscription-price-in-different-countries")
+
+print("Path to dataset files:", path)
+
+
+
+
+
+
+
+
