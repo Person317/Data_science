45751c7b1c8840a1ca8f54e2847213748a74bb75
Person317 <gemiyah317@gmail.com>
2024-12-10 10:56:44 -0500

    Adding on

--- Main.py
+++ Main.py
@@ -1,12 +1,21 @@
 
 
 import pandas as pd
+df = pd.read_csv ("data.csv")
+import matplotlib.pyplot as plt
 
-df = pd.read_csv (r"data.csv")
+# Cleaning data
+print("List of missing column values")
+print("-------------------------")
+print(df.isnull().sum())
 
+print()
+print("Number of duplicate rows")
+print("-------------------------")
+print(df.duplicated().sum())
 
-
-
-
-
+# 1st Pie chart
+df1 = df.groupby("Name").sum()
+df1.plot.pie(y="GDP", lables=df1.index)
+plt.show()
 
