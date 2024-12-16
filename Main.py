46051cd259f9d37540a8ddb50e570af7c0a64df7
Person317 <gemiyah317@gmail.com>
2024-12-16 16:59:33 -0500

    More stuff

--- Main.py
+++ Main.py
@@ -15,5 +15,15 @@ print("Number of duplicate rows")
 print("-------------------------")
 print(df.duplicated().sum())
 
+#Pie chart 1
+y = np.array([39, 36.3, 33.1, 25.2, 21.5])
+mylabels = ["Boliva", "Mexico", "Venezuela", "Ecuador", "New Zeland"]
+plt.pie(y, labels = mylabels)
+plt.legend(title = "Highest Poverty Rates")
+plt.show()
 
-y = np.array(["Argintina"])
+y = np.array([10.99, 10.43, 10.99, 10.99, 12.53])
+mylabels = ["Boliva", "Mexico", "Venezuela", "Ecuador", "New Zeland"]
+plt.pie(y, labels = mylabels)
+plt.legend(title = "Corresponding Subscription Rates")
+plt.show()
