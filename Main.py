7a03280f8166a0c4947a4c4b86d14c7abacdeb0d
Person317 <gemiyah317@gmail.com>
2024-12-16 18:03:04 -0500

    Finally the graph code is done

--- Main.py
+++ Main.py
@@ -22,8 +22,28 @@ plt.pie(y, labels = mylabels)
 plt.legend(title = "Highest Poverty Rates")
 plt.show()
 
+#Pie chart 2
 y = np.array([10.99, 10.43, 10.99, 10.99, 12.53])
 mylabels = ["Boliva", "Mexico", "Venezuela", "Ecuador", "New Zeland"]
 plt.pie(y, labels = mylabels)
 plt.legend(title = "Corresponding Subscription Rates")
+plt.show()
+
+#Bar Graph 1
+x = np.array(["United States", "Hong Kong", "Germany", " Australia", "France"])
+y = np.array([73600, 64400, 61900, 59500, 55200])
+plt.bar(x,y)
+plt.show()
+
+#Bar Graph 2
+x = np.array(["United States", "Hong Kong", "Germany", " Australia", "France"])
+y = np.array([13.99, 11.93, 14.67, 12.12, 15.24])
+plt.bar(x,y)
+plt.show()
+
+#Line Graph
+x = np.array([123.2, 68.4, 52.0, 10.8, 9.4, 6.7])
+y = np.array([13.13, 13.2, 11.47, 11.49, 19.54, 11.29])
+plt.plot(x)
+plt.plot(y)
 plt.show()
